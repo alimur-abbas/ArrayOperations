8c05e36e3acdb4b09743d798fc8ca60307ff5711
alimur-abbas <alimurabbas8658402111@gmail.com>
2021-12-22 23:24:23 +0530

    Array Implementation of Queue

---
+++ src/QueueImplementation.java
@@ -0,0 +1,138 @@
+import java.util.*;
+public class QueueImplementation {
+    int size ;
+
+    int front, rear;
+    int items [] = new int[100];
+    Scanner sc = new Scanner(System.in);
+
+     void createQueue(){
+
+        System.out.println("Enter the Size of Queueu");
+        size = sc.nextInt();
+        items = new int[size];
+    }
+    void inputToQueue()
+    {
+        for(int i = 1; i < 6; i ++) {
+            System.out.println("Enter the Element");
+            int element = sc.nextInt();
+            enQueue(element);
+        }
+    }
+
+
+
+
+    QueueImplementation() {
+        front = -1;
+        rear = -1;
+    }
+
+    // check if the queue is full
+    boolean isFull() {
+        if (front == 0 && rear == size - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    // check if the queue is empty
+    boolean isEmpty() {
+        if (front == -1)
+            return true;
+        else
+            return false;
+    }
+
+    // insert elements to the queue
+    void enQueue(int element) {
+
+        // if queue is full
+        if (isFull()) {
+            System.out.println("Queue is full");
+        }
+        else {
+            if (front == -1) {
+                // mark front denote first element of queue
+                front = 0;
+            }
+
+            rear++;
+            // insert element at the rear
+            items[rear] = element;
+            System.out.println("Insert " + element);
+        }
+    }
+
+    // delete element from the queue
+    int deQueue() {
+        int element;
+
+        // if queue is empty
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        }
+        else {
+            // remove element from the front of queue
+            element = items[front];
+
+            // if the queue has only one element
+            if (front >= rear) {
+                front = -1;
+                rear = -1;
+            }
+            else {
+                // mark next element as the front
+                front++;
+            }
+            System.out.println( element + " Deleted");
+            return (element);
+        }
+    }
+
+    // display element of the queue
+    void display() {
+        int i;
+        if (isEmpty()) {
+            System.out.println("Empty Queue");
+        }
+        else {
+            // display the front of the queue
+            System.out.println("\nFront index-> " + front);
+
+            // display element of the queue
+            System.out.println("Items -> ");
+            for (i = front; i <= rear; i++)
+                System.out.print(items[i] + "  ");
+
+            // display the rear of the queue
+            System.out.println("\nRear index-> " + rear);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // create an object of Queue class
+        QueueImplementation q = new QueueImplementation();
+
+
+        q.createQueue();
+        q.deQueue();// Queue is Empty message will be displayed as queue is empty
+        q.inputToQueue();
+        q.enQueue(58);// next element can't be added to queue because queue is full
+        q.display();
+        q.deQueue();// deQueue removes element entered first Element , with deleted element mesaage
+        q.display(); // Now we have just size-1 elements
+
+    }
+}
+
+
+
+
+
+
+
+
